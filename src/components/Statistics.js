f536a2776fec5061f6128631ec491f623e66188b
Michael Kosak <stu97308@mail.uni-kiel.de>
2018-06-19 15:59:21 +0200

    Sortfuntion für Datum eingefügt

--- src/components/Statistics.js
+++ src/components/Statistics.js
@@ -2,8 +2,24 @@ import React, { Component } from 'react'
 
 export default class Statistics extends Component {
   render() {
+    function backToDatum(datum) {
+      const Punkt = '.'
+      const pos = 2
+      const teil1 = datum.substr(0, pos)
+      const teil2 = datum.substr(pos, datum.length - pos)
+      const teil3 = teil2.substr(0, pos)
+      const teil4 = teil2.substr(pos, teil2.length - pos)
+      return teil1 + Punkt + teil3 + Punkt + teil4
+    }
+
     const { history, habits } = this.props
-    return Object.keys(history).map(dateKey => {
+
+    let newArray = Object.keys(history).map(e =>
+      e.replace('.', '').replace('.', '')
+    )
+    const newerArray = newArray.sort((a, b) => b - a).map(e => backToDatum(e))
+
+    return newerArray.map(dateKey => {
       const dateEntries = history[dateKey]
       return (
         <div key={dateKey}>
